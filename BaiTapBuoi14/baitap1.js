302baa8f0f39f4bf62bd21e0eaa9326175aedae5
Tdxyz053181 <nguyentiendat.151202@gmail.com>
2026-06-22 21:31:50 +0700

    Done bt 1

--- BaiTapBuoi14/baitap1.js
+++ BaiTapBuoi14/baitap1.js
@@ -1,1 +1,13 @@
+function analyzeValue(value) {
+    return {
+        input: value,
+        type: typeof value,
+        isTruthy: Boolean(value),
+        isNullOrUndefined: value === null || value === undefined,
+        isReferenceType:
+            (typeof value === "object" && value !== null) ||
+            typeof value === "function"
+    };
+}
 
+console.log(analyzeValue(null));
