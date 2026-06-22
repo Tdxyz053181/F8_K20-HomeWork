0de6f60984980f182ef6e640904e4ec972c0b2ac
Tdxyz053181 <nguyentiendat.151202@gmail.com>
2026-06-22 21:37:58 +0700

    Done bt 2

--- BaiTapBuoi14/baitap2.js
+++ BaiTapBuoi14/baitap2.js
@@ -1,1 +1,22 @@
+function calculateScore(level, kills, boosted) {
+    if (
+        typeof level !== "number" || typeof kills !== "number" || level < 0 || kills < 0
+    ) {
+        return "Dữ liệu không hợp lệ";
+    }
 
+    if (typeof boosted !== "boolean") {
+        boosted = false;
+    }
+
+    const baseScore = kills * 10;
+
+    const bonusScore =
+        level >= 5 ? baseScore * 0.5 : baseScore * 0.2;
+
+    const finalScore = boosted ? (baseScore + bonusScore) * 2 : baseScore + bonusScore;
+
+    return Math.floor(finalScore);
+}
+
+console.log(calculateScore(5, 20, true))
